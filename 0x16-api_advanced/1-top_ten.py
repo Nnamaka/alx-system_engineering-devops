a44fc3a540047884d181a0d5ea8194e612b668c9
Nnamaka <nnamaka7@gmail.com>
2023-05-10 06:37:28 -0700

    he did

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,8 +6,9 @@ def top_ten(subreddit):
     """ returns top 10 post """
 
     api = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    header = {'User-Agent' : 'priceless stuff'}
 
-    res = requests.get(api, params=10)
+    res = requests.get(api, headers=header, params={'limit':10}, allow_redirects=False)
 
     if res.status_code != 200:
         print('None')
