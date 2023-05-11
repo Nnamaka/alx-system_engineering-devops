28ea9beaf901d9110a7f2043813f73a4ef77746b
Nnamaka <nnamaka7@gmail.com>
2023-05-11 02:46:08 -0700

    the foundation

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # change typo errors from 'phpp' to 'php'
 
 exec { 'Adjust typos':
-	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	path => '/usr/local/bin/:/bin/'
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
